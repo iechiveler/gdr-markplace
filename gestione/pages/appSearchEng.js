5e3c40f4f99867ca388c5a42e817ee93a0302773
iechiveler <ultra704@hotmail.it>
2022-04-04 01:01:23 +0200

    script search

--- gestione/pages/appSearchEng.js
+++ gestione/pages/appSearchEng.js
@@ -2,15 +2,30 @@ const button = document.querySelectorAll(".lebutton");
 
 button.forEach(btn => {
     btn.addEventListener('click', function (eventi) {
-        rowbtn = eventi.target;
-        row = eventi.target.parentNode.parentNode;
-        val = row.querySelectorAll("#view");
+        const row = eventi.target.parentNode.parentNode;
+        const val = row.querySelectorAll("#view");
 
         val.forEach(vals => {
             vals.removeAttribute("disabled");
-            console.log(vals);
+            
         });
 
+        hideBut();
+        newBut();
+
+        function hideBut() {
+            btn.setAttribute("hidden", "");
+        };
+
+        function newBut() {
+            const newBtn = document.createElement("button");
+            newBtn.innerHTML = "Aggiorna";
+            newBtn.name = "addValues";
+            newBtn.type = "submit";
+            newBtn.className = "btn btn-success"
+            btn.parentElement.append(newBtn);
+        };
+        
     });
 
 });
